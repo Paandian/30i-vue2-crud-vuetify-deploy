de7d575fb3f684f26e1f7879c92105ed2db21992
Paandian <85572497+Paandian@users.noreply.github.com>
2021-08-26 16:21:20 +0800

    update

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ require('./app/routes/user.routes')(app);
 require('./app/routes/tutorial.routes')(app);
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8087;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
